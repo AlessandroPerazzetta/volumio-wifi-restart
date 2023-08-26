d064b2ce1d656e099dc96c980f32de41b18ea392
Alessandro Perazzetta <alessandro.perazzetta@dmiweb.net>
2023-08-26 14:50:40 +0200

    Add commands args, removed default play

--- README.md
+++ README.md
@@ -8,4 +8,10 @@
 $ crontab -e
 ```
 
+### Restart wifi only:
+
 **`*/3 * * * * /home/volumio/scripts/wifi-restart.sh >/dev/null 2>&1`**
+
+### Restart wifi and play:
+
+**`*/3 * * * * /home/volumio/scripts/wifi-restart.sh play>/dev/null 2>&1`**
--- wifi-restart.sh
+++ wifi-restart.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
+cmd=$1
 ssid=$(/sbin/iwgetid --raw)
 if [ -z "$ssid" ]
 then
@@ -6,6 +7,15 @@ then
     /sbin/ifconfig wlan0 down
     sleep 5
     systemctl restart wireless
-    /volumio/app/plugins/system_controller/volumio_command_line_client/volumio.sh play
+    #/volumio/app/plugins/system_controller/volumio_command_line_client/volumio.sh play
+    if [ -z "$cmd" ]
+    then
+        echo "Command is empty"
+    else
+        echo "Command: $cmd"
+        if [ $cmd = "play" ]; then
+            /volumio/app/plugins/system_controller/volumio_command_line_client/volumio.sh play
+        fi
+    fi
 fi
 echo "wifi-check done"
